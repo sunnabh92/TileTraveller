31e024ffba02e765afcb612ed996322f85ceeea1
Sunna <sunnah19@ru.is>
2019-09-17 13:50:00 +0000

    First commit

---
+++ tile_traveller.py
@@ -0,0 +1,8 @@
+# Making a game that has 9 tiles. Main goal is to move from tile 1,1 to 3,1.
+# Make function for begining location.
+# For each tile the program prints out in which direciton you can move.
+# Function for the winner.
+# For each tile, the function must alert if the direction is invalid and keep still in the same tile. If the direction that is available is chosen then move to that direction.
+# Have the functions for each direction not each tile.
+# Program has to track current loaction and future locations, print out available direcitons for the right tile and ask for direction.
+
